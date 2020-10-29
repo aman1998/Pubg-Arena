7560a3b192953633d32fd08b6e3e95420e41093d
amangeldi <1998-amangeldi@mail.ru>
2020-10-29 16:43:05 +0600

    validation bug fixed

--- src/components/UI/Login/Template.js
+++ src/components/UI/Login/Template.js
@@ -1,6 +1,7 @@
 import React  from 'react'
 import { Formik, Form, Field, ErrorMessage } from 'formik';
 import Header from './Header'
+import * as Yup from 'yup';
 
 const Login = (props) => {
 
@@ -8,30 +9,30 @@ const Login = (props) => {
     <div className='login'>
       <Header title={props.title} bg={props.bg} close={props.close}/>
       <Formik
-        initialValues={{ email: '', password: '', name: '' }}
-        validate={values => {
-          const errors = {};
-          if (!values.email || !values.password || !values.name) {
-            errors.email = 'Это поле обязательно!';
-            errors.password = 'Введите свой пароль!';
-            errors.name = 'Введите свой никнейм!';
-          } else if (
-            !/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i.test(values.email) 
-            // &&
-            // !values.password.length < 3 && values.password.length >= 10 
-          ) {
-            errors.email = 'Неправильный формат';
-            // errors.password = 'Пароль должен быть включать 4-10 символов';
-            // errors.name = 'Имя должно быть включать 4-10 символов';
-          }
-          return errors;
+        initialValues={{ 
+          login: '',
+          email: '',
+          password: '',
+          confirmPassword: '',
         }}
-        onSubmit={(values, { setSubmitting }) => {
-          setTimeout(() => {
-            alert(JSON.stringify(values, null, 2));
-            setSubmitting(false);
-          }, 400);
-        }}
+        validationSchema={Yup.object().shape({
+          title: Yup.string()
+              .required('Title is required'),
+          login: Yup.string()
+              .required('Введите никнейм!'),
+          email: Yup.string()
+              .email('Неверный формат')
+              .required('Введите почту'),
+          password: Yup.string()
+              .min(6, 'В пароле должно быть минимум 6 символов!')
+              .required('Введите пароль!'),
+          confirmPassword: Yup.string()
+              .oneOf([Yup.ref('password'), null], 'Пароли не совпадают!')
+              .required('Подтвердите пароль!'),
+      })}
+      onSubmit={fields => {
+        alert('SUCCESS!! :-)\n\n' + JSON.stringify(fields, null, 4))
+    }}
       >
         {({ isSubmitting }) => (
           <Form className='loginForm'>
@@ -42,10 +43,12 @@ const Login = (props) => {
               </div>
               : ''
             }
-            <Field type="text" name="name" placeholder='Никнейм'/>
-            <ErrorMessage name="name" component="div" className='error'/>
+            <Field type="text" name="login" placeholder='Никнейм'/>
+            <ErrorMessage name="login" component="div" className='error'/>
             <Field type="password" name="password" placeholder='Пароль'/>
             <ErrorMessage name="password" component="div" className='error'/>
+            <Field type="password" name="confirmPassword" placeholder='Повторите пароль'/>
+            <ErrorMessage name="confirmPassword" component="div" className='error'/>
             <button type="submit" disabled={isSubmitting} className='loginFormBtn' style={{background: props.bg}}>
               {props.title}
             </button>
