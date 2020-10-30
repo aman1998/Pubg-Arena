a8326b55611f3dfdeb421ea8a1201494881ed4eb
erlan artykbaev <erik.2405@mail.ru>
2020-10-30 16:34:41 +0600

    just modify redux

--- src/store/actionTypes.js
+++ src/store/actionTypes.js
@@ -1,11 +1,9 @@
-export const GET_USERNAME = 'GET_USERNAME'
-export const GET_EMAIL = 'GET_EMAIL'
-export const GET_PASSWORD = 'GET_PASSWORD'
 export const GET_ALL = 'GET_ALL'
 export const SET_USERNAME = 'SET_USERNAME'
 export const SET_EMAIL = 'SET_EMAIL'
 export const SET_PASSWORD = 'SET_PASSWORD'
 export const NEW_USER = 'NEW_USER'
+export const GET_TOKEN = 'GET_TOKEN'
 
 export const IS_LOGGED = 'IS_LOGGED'
 export const LOG_IN = 'LOG_IN'
--- src/store/reducers/auth.js
+++ src/store/reducers/auth.js
@@ -1,30 +1,24 @@
 import {
   GET_ALL,
-  GET_EMAIL,
-  GET_PASSWORD,
-  GET_USERNAME, NEW_USER,
+  GET_TOKEN,
   SET_EMAIL,
   SET_PASSWORD,
   SET_USERNAME
 } from "../actionTypes";
 
-const initialState = [
-  {
-    username: 'admin',
-    email: 'admin@test.com',
-    password: 'adminadmin'
-  }
-]
+const initialState = {
+  username: 'admin',
+  email: 'admin@test.com',
+  password: 'adminadmin',
+  token: 'akkahahha'
+}
 
 const reducer = (state = initialState, action) => {
   switch (action.type) {
     case GET_ALL:
       return state
-    case NEW_USER:
-      return [
-        ...state,
-        action.payload
-      ]
+    case GET_TOKEN:
+      return state.token
     case SET_USERNAME:
       return {
         ...state,
