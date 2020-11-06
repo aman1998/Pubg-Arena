4ec6276293950505d877b5bac85e4ad68218b894
erlan artykbaev <erik.2405@mail.ru>
2020-11-06 12:36:25 +0600

    bugfix: render first buttons and then login

--- src/store/reducers/isLogged.js
+++ src/store/reducers/isLogged.js
@@ -1,6 +1,14 @@
 import {IS_LOGGED, LOG_IN, LOG_OUT} from "../actionTypes";
 
-const reducer = (state = false, action) => {
+const searchToken = () => {
+  const isToken = localStorage.getItem('token')
+  if(isToken){
+    return true
+  }
+  return false
+}
+
+const reducer = (state = searchToken, action) => {
   switch (action.type) {
     case IS_LOGGED:
       return state
