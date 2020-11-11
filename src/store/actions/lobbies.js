15ce8de9e0c0bb13acb54dacffdd796f8bc40e43
erlan artykbaev <erik.2405@mail.ru>
2020-11-11 11:53:34 +0600

    feature: move fetch requests of login and lobbies to DAL

--- src/store/actions/lobbies.js
+++ src/store/actions/lobbies.js
@@ -6,11 +6,14 @@ export const setLobbiesList = (list) => ({
   list
 })
 
-export const fetchLobbiesActionCreator = () => {
-  return async dispatch => {
-    const response = await axios.get('/lobby/rates/')
-    dispatch(setLobbiesList(response.data))
-  }
+export const fetchLobbiesActionCreator = () => dispatch => {
+  axios.get('/lobby/rates/')
+    .then(response => {
+      dispatch(setLobbiesList(response.data))
+    })
+    .catch(error => {
+      console.log(error)
+    })
 }
 
 export const setCurrent = (current) => ({
