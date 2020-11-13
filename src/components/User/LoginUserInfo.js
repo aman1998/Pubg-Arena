d3928d289aabcf0cf40cb6f3219928981251f3cf
erlan artykbaev <erik.2405@mail.ru>
2020-11-13 12:06:05 +0600

    feature: show users' info to header when window.size <= 800 or hide info and use only avatar

--- src/components/User/LoginUserInfo.js
+++ src/components/User/LoginUserInfo.js
@@ -1,27 +1,56 @@
 import React, {useState} from 'react'
-import {Link} from "react-router-dom";
+import {Link, NavLink} from "react-router-dom"
+import useWindowDimensions from "../Hooks/useWindowDimentions"
+
 import PersonIcon from '@material-ui/icons/Person'
 import ExitToAppIcon from '@material-ui/icons/ExitToApp'
-import { NavLink } from 'react-router-dom'
+import MonetizationOnIcon from '@material-ui/icons/MonetizationOn'
 
 const LoginUserInfo = (props) => {
   const [modalUserInfo, setModalUserInfo] = useState(false)
+  const {width} = useWindowDimensions()
 
   return (
     <div className='loginUserInfo'>
-      <div className='userAvatar' onClick={() => setModalUserInfo(!modalUserInfo)}>
-        <PersonIcon className='userIcon'/>
+      <div
+        className='userAvatar'
+        onClick={() => setModalUserInfo(!modalUserInfo)}
+      >
+        {
+          width >= 800 ? (
+            <>
+              <div className='username'>
+                <div>{props.name}</div>
+                <PersonIcon className='userIcon'/>
+              </div>
+              <div className='balance'>
+                <div> {props.balance} </div>
+                <MonetizationOnIcon className='balance-icon'/>
+              </div>
+            </>
+          ) : (
+            <div className='username'>
+              <PersonIcon className='userIcon'/>
+            </div>
+          )
+        }
       </div>
       <div className={modalUserInfo ? 'modalInfo down' : 'modalInfo up'}>
-        <div className='phone'>
-          {props.phone}
-        </div>
-        <div className='username'>
-          {props.name}
-        </div>
-        <div className='money'>
-          balance: {props.balance}
-        </div>
+        {
+          width <= 800 ? (
+            <>
+              <div className='phone'>
+                {props.phone}
+              </div>
+              <div className='username'>
+                {props.name}
+              </div>
+              <div className='money'>
+                balance: {props.balance}
+              </div>
+            </>
+          ) : null
+        }
         <div className='sidebar-mobile'>
           <div><NavLink to='/profile/' exact>Личные данные</NavLink></div>
           <div><NavLink to='/profile/settings/' exact>Настройки</NavLink></div>
