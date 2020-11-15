1760dd1aa9fa444f2b487c9905dce7284443f6eb
erlan artykbaev <erik.2405@mail.ru>
2020-11-15 19:41:51 +0600

    feature: footer is ready, adaptive and cool

--- src/components/layout/Footer.js
+++ src/components/layout/Footer.js
@@ -1,13 +1,38 @@
 import React from 'react'
-import Logo from '../../assets/icons/logo.svg'
-import Vk from '../../assets/icons/vkontakte'
-import Tg from '../../assets/icons/telegram'
+import FacebookIcon from '@material-ui/icons/Facebook'
+import InstagramIcon from '@material-ui/icons/Instagram'
+import TwitterIcon from '@material-ui/icons/Twitter'
+import {NavLink} from "react-router-dom";
 
-const Footer = (props) => {
-  return(
-      <footer className={`footer`}>
-        
-      </footer>
+
+const Footer = () => {
+  return (
+    <footer className='footer'>
+      <div className="container">
+        <div className='footer-logo'>
+          <h2>LOGO</h2>
+        </div>
+        <div className='footer-main'>
+          <div className='title'>Главная</div>
+          <div className='text'>Условия пользования</div>
+        </div>
+        <div className='footer-private'>
+          <div className='title'>Личный кабинет</div>
+          <div className='text'>Политика конфидициальности</div>
+        </div>
+        <div className="footer-soc">
+          <NavLink to='https://facebook.com' >
+            <FacebookIcon className='facebook-icon'/>
+          </NavLink>
+          <NavLink to='https://instagram.com'>
+            <InstagramIcon className='insta-icon'/>
+          </NavLink>
+          <NavLink to='https://twitter.com'>
+            <TwitterIcon className='twitter-icon'/>
+          </NavLink>
+        </div>
+      </div>
+    </footer>
   )
 }
 
