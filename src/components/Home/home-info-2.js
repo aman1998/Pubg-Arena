030d7e5378b6621ecebba797de7b596fc856406f
amangeldi <1998-amangeldi@mail.ru>
2020-11-23 10:48:24 +0600

    feature: home

--- src/components/Home/home-info-2.js
+++ src/components/Home/home-info-2.js
@@ -1,8 +1,58 @@
 import React from 'react'
 
+import Phone from '../../assets/img/phone-2.png'
+import ImgShiet from '../../assets/icons/shiet.svg'
+import ImgTrend from '../../assets/icons/trending-up.svg'
+import ImgUsers from '../../assets/icons/user-plus.svg'
+import ImgEdit from '../../assets/icons/edit-3.svg'
+
 const HomeInfo2 = () => {
   return (
-    <section>sdsd</section>
+    <section className='info-content-2 container'>
+      <h1>Вся жизнь - игра</h1>
+      <div className='content'>
+        <div>
+          <img src={Phone} alt='#' className='phone'/>
+          <div className='text-item rate-mobile'>
+            <img src={ImgTrend} alt='#' />
+            <div>Рейтинг</div>
+            <p>
+            Играй  и займи место в дневном, недельном или ежемесячном рейтинге   
+            </p>
+          </div>
+        </div>
+        <div className='info-text'>
+          <div className='text-item'>
+            <img src={ImgEdit} alt='#'/>
+            <div>Регистрация</div>
+            <p>
+            Аккаунт создается удобным для вас способом   
+            </p>
+          </div>
+          <div className='text-item'>
+            <img src={ImgUsers} alt='#' />
+            <div>Играй сам или в команде</div>
+            <p>
+            Проводим большое количество турниров для всех режимов  
+            </p>
+          </div>
+          <div className='text-item'>
+            <img src={ImgShiet} alt='#' />
+            <div>Разнообразные форматы</div>
+            <p>
+            Турниры на вхождение в топ, турниры на килы. Всегда есть выбор 
+            </p>
+          </div>
+          <div className='text-item rate'>
+            <img src={ImgTrend} alt='#' />
+            <div>Рейтинг</div>
+            <p>
+            Играй  и займи место в дневном, недельном или ежемесячном рейтинге   
+            </p>
+          </div>
+        </div>
+      </div>
+    </section>
   )
 }
 
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -3,7 +3,9 @@ import PageTemplate from '../components/templates/PageTemplate'
 
 import HeaderContent from '../components/Home/header-content'
 import HomeInfo1 from '../components/Home/home-info-1'
+import HomeInfo2 from '../components/Home/home-info-2'
 import CardIn from '../components/Home/cardIn'
+import Questions from '../components/Home/questions'
 
 const Home = () => {
   return (
@@ -12,6 +14,8 @@ const Home = () => {
         <HeaderContent />
         <HomeInfo1 />
         <CardIn />
+        <HomeInfo2 />
+        <Questions />
       </div>
     </PageTemplate>
   )
