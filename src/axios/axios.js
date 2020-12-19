de0321c4ef56dee434ff7eba3e04c192c1d9d8ff
erlan artykbaev <erik.2405@mail.ru>
2020-12-19 17:00:27 +0600

    bugfix: must and something

--- src/axios/axios.js
+++ src/axios/axios.js
@@ -2,6 +2,6 @@ import axios from 'axios'
 //first to prod
 //second to test back
 export default axios.create({
-  // baseURL: 'http://195.38.164.24:8080'
-  baseURL: 'http://193.124.206.13:8080'
+  baseURL: 'http://195.38.164.24:8080'
+  // baseURL: 'http://193.124.206.13:8080'
 })
