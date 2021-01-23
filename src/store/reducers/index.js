ff82bcd297e2d537e8e40a0cbc7fb65fb6600ab9
user <user@users-MacBook-Air.local>
2021-01-23 12:16:04 +0600

    push project

--- src/store/reducers/index.js
+++ src/store/reducers/index.js
@@ -1,32 +1,7 @@
 import { combineReducers } from 'redux'
 import profile from './profile'
-import isLogged from './isLogged'
-import modalRegister from './modalRegister'
-import modalLogin from './modalLogin'
-import lobbies from './lobbies'
-import register from './register'
-import isLoading from './isLoading'
-import isAuthState from './isAuthState'
-import lobby from './lobby'
-import timer from './timer'
-import rates from './rates'
-import fetch from './fetch'
-import language from './language'
-import card from './card'
 
 export default combineReducers({
   profile,
-  isLogged,
-  modalRegister,
-  modalLogin,
-  lobbies,
-  isLoading,
-  register,
-  isAuthState,
-  lobby,
-  timer,
-  rates,
-  fetch,
-  language,
-  card
+
 })
